6729f3c95602ba4cbfe78492536b378f1ee8d476
Itamar-Manor <1manoritamar@gmail.com>
2024-11-24 22:41:00 +0200

    Changed output to fit requirements

--- Primes.java
+++ Primes.java
@@ -22,7 +22,7 @@ public class Primes {
                 }
             }
         }
-        System.out.println("Primes up to " + n + ":");
+        System.out.println("Prime numbers up to " + n + ":");
         int count = 0;
         for (int i = 0; i < primes.length; i++) {
             if (primes[i]) {
